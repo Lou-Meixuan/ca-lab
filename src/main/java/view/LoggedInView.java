f5ad4449084d8bf04ea2e91e55dac479086d23bf
denizcoban1 <d.coban@mail.utoronto.ca>
2025-10-20 13:51:56 -0400

    implemented setLogoutController

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -108,7 +108,10 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
      * @param evt the ActionEvent to react to
      */
     public void actionPerformed(ActionEvent evt) {
-        System.out.println("Click " + evt.getActionCommand());
+        // System.out.println("Click " + evt.getActionCommand());
+        if (evt.getSource() == logOut && logoutController != null) {
+            logoutController.execute();
+        }
     }
 
     @Override
@@ -140,5 +143,6 @@ public class LoggedInView extends JPanel implements ActionListener, PropertyChan
 
     public void setLogoutController(LogoutController logoutController) {
         // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
